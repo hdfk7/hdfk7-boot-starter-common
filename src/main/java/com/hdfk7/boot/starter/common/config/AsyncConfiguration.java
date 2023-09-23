ae6777350d04b6e0b41fc153e8930f6192cdb59f
qiang.liang <173228514@qq.com>
2023-09-23 21:43:44 +0800

    feat:新增线程池配置

--- src/main/java/com/hdfk7/boot/starter/common/config/AsyncConfiguration.java
+++ src/main/java/com/hdfk7/boot/starter/common/config/AsyncConfiguration.java
@@ -2,6 +2,7 @@ package com.hdfk7.boot.starter.common.config;
 
 import cn.hutool.extra.spring.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,35 +12,10 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.Optional;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
-public class AsyncConfiguration {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            log.info("AsyncTaskExecutor - Shutdown initiated...");
-
-            ThreadPoolTaskExecutor shortTaskExecutor = SpringUtil.getBean("shortTaskExecutor", ThreadPoolTaskExecutor.class);
-            Optional.ofNullable(shortTaskExecutor).ifPresent(ExecutorConfigurationSupport::shutdown);
-
-            ThreadPoolTaskExecutor longTaskExecutor = SpringUtil.getBean("longTaskExecutor", ThreadPoolTaskExecutor.class);
-            Optional.ofNullable(longTaskExecutor).ifPresent(ExecutorConfigurationSupport::shutdown);
-
-            while ((shortTaskExecutor != null && shortTaskExecutor.getActiveCount() > 0)
-                    || (longTaskExecutor != null && longTaskExecutor.getActiveCount() > 0)) {
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    log.error(e.getLocalizedMessage(), e);
-                    break;
-                }
-            }
-
-            log.info("AsyncTaskExecutor - Shutdown completed.");
-        }));
-    }
-
+public class AsyncConfiguration implements DisposableBean {
     @Bean
     @Primary
     public TaskExecutor shortTaskExecutor() {
@@ -48,6 +24,7 @@ public class AsyncConfiguration {
         executor.setCorePoolSize(core);
         executor.setMaxPoolSize(core * 2 + 1);
         executor.setKeepAliveSeconds(60);
+        executor.setAwaitTerminationMillis(1000L * 60);
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("short-task-executor-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
@@ -63,11 +40,21 @@ public class AsyncConfiguration {
         executor.setCorePoolSize(core);
         executor.setMaxPoolSize(core * 10 + 1);
         executor.setKeepAliveSeconds(60);
+        executor.setAwaitTerminationMillis(1000L * 60);
         executor.setQueueCapacity(500);
         executor.setThreadNamePrefix("long-task-executor-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        ThreadPoolTaskExecutor shortTaskExecutor = SpringUtil.getBean("shortTaskExecutor", ThreadPoolTaskExecutor.class);
+        Optional.ofNullable(shortTaskExecutor).ifPresent(ExecutorConfigurationSupport::destroy);
+
+        ThreadPoolTaskExecutor longTaskExecutor = SpringUtil.getBean("longTaskExecutor", ThreadPoolTaskExecutor.class);
+        Optional.ofNullable(longTaskExecutor).ifPresent(ExecutorConfigurationSupport::destroy);
     }
 }
