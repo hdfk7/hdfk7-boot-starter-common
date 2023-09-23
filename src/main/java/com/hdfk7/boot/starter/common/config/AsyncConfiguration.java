a4a397275ba6e3becfbf298dcf0eee0ec5e7bf75
qiang.liang <173228514@qq.com>
2023-09-23 20:42:13 +0800

    feat:新增线程池配置

---
+++ src/main/java/com/hdfk7/boot/starter/common/config/AsyncConfiguration.java
@@ -0,0 +1,62 @@
+package com.hdfk7.boot.starter.common.config;
+
+import cn.hutool.extra.spring.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ExecutorConfigurationSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Slf4j
+@Configuration
+public class AsyncConfiguration {
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("AsyncTaskExecutor - Shutdown initiated...");
+
+            ThreadPoolTaskExecutor shortTaskExecutor = SpringUtil.getBean("shortTaskExecutor", ThreadPoolTaskExecutor.class);
+            Optional.ofNullable(shortTaskExecutor).ifPresent(ExecutorConfigurationSupport::shutdown);
+
+            ThreadPoolTaskExecutor longTaskExecutor = SpringUtil.getBean("longTaskExecutor", ThreadPoolTaskExecutor.class);
+            Optional.ofNullable(longTaskExecutor).ifPresent(ExecutorConfigurationSupport::shutdown);
+
+            log.info("AsyncTaskExecutor - Shutdown completed.");
+        }));
+    }
+
+    @Bean
+    @Primary
+    public TaskExecutor shortTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int core = Runtime.getRuntime().availableProcessors();
+        executor.setCorePoolSize(core);
+        executor.setMaxPoolSize(core * 2 + 1);
+        executor.setKeepAliveSeconds(60);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("short-task-executor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public TaskExecutor longTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int core = Runtime.getRuntime().availableProcessors();
+        executor.setCorePoolSize(core);
+        executor.setMaxPoolSize(core * 10 + 1);
+        executor.setKeepAliveSeconds(60);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("long-task-executor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+}
