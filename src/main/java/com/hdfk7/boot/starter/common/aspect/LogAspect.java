2236b74fcad327dfe43ad189f8ed3da3c210707d
qiang.liang <lq173228514@gmail.com>
2024-06-27 13:13:32 +0800

    优化http请求日志输出

--- src/main/java/com/hdfk7/boot/starter/common/aspect/LogAspect.java
+++ src/main/java/com/hdfk7/boot/starter/common/aspect/LogAspect.java
@@ -1,5 +1,6 @@
 package com.hdfk7.boot.starter.common.aspect;
 
+import cn.hutool.core.util.ObjUtil;
 import cn.hutool.json.JSONUtil;
 import com.hdfk7.boot.starter.common.constants.RequestParamConst;
 import com.hdfk7.boot.starter.common.util.IpUtil;
@@ -20,8 +21,6 @@ import java.util.Objects;
 
 @Slf4j
 public abstract class LogAspect {
-    private static final String ERROR_PATH_METHOD_NAME = "getErrorPath";
-    private static final String HEAD = "HEAD";
 
     public void init(JoinPoint joinPoint) {
         ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
@@ -29,27 +28,26 @@ public abstract class LogAspect {
             return;
         }
 
-        HttpServletRequest request = sra.getRequest();
-        Object[] pointArgs = joinPoint.getArgs();
         List<Object> argList = new ArrayList<>();
-        for (Object object : pointArgs) {
-            if (!(object instanceof BindingResult
-                    || object instanceof ServletRequest
-                    || object instanceof ServletResponse
-                    || object instanceof MultipartFile)) {
-                argList.add(object);
+        for (Object arg : joinPoint.getArgs()) {
+            if (!(arg instanceof BindingResult
+                    || arg instanceof ServletRequest
+                    || arg instanceof ServletResponse
+                    || arg instanceof MultipartFile)) {
+                argList.add(arg);
             }
         }
+        String parameters = JSONUtil.toJsonStr(argList);
+        HttpServletRequest request = sra.getRequest();
         request.setAttribute(RequestParamConst.REQUEST_START_TIME, System.currentTimeMillis());
         request.setAttribute(RequestParamConst.METHOD_NAME, joinPoint.getSignature().getName());
-        request.setAttribute(RequestParamConst.PARAMETERS, JSONUtil.toJsonStr(argList));
+        request.setAttribute(RequestParamConst.PARAMETERS, parameters);
 
-        String methodName = (String) request.getAttribute(RequestParamConst.METHOD_NAME);
-        if (!ignoreRequest(methodName, sra.getRequest().getMethod())) {
-            String url = sra.getRequest().getRequestURL().toString();
-            String httpMethod = sra.getRequest().getMethod();
-            log.info(String.format("requestBegin: url[%s],httpMethod[%s],request[%s]", url, httpMethod, JSONUtil.toJsonStr(argList)));
-        }
+        String url = sra.getRequest().getRequestURL().toString();
+        String method = sra.getRequest().getMethod();
+        String host = IpUtil.getIpAddress(request);
+        int port = request.getRemotePort();
+        log.info(String.format("%s|%s|%s|%d", method, url, host, port));
     }
 
     public Object doTask(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -62,35 +60,25 @@ public abstract class LogAspect {
             return;
         }
 
-        String response = "";
-        if (Objects.nonNull(ret)) {
-            response = JSONUtil.toJsonStr(ret);
-        }
+        String response = ObjUtil.isNotNull(ret) ? JSONUtil.toJsonStr(ret) : "-";
         HttpServletRequest request = sra.getRequest();
-        String methodName = (String) request.getAttribute(RequestParamConst.METHOD_NAME);
         String parameters = (String) request.getAttribute(RequestParamConst.PARAMETERS);
+        parameters = ObjUtil.isNotNull(parameters) ? parameters : "-";
         Object startTime = request.getAttribute(RequestParamConst.REQUEST_START_TIME);
         request.removeAttribute(RequestParamConst.METHOD_NAME);
         request.removeAttribute(RequestParamConst.PARAMETERS);
         request.removeAttribute(RequestParamConst.REQUEST_START_TIME);
-        if (ERROR_PATH_METHOD_NAME.equalsIgnoreCase(methodName) || HEAD.equalsIgnoreCase(methodName)) {
-            return;
-        }
 
         long requestStartTime = Objects.nonNull(startTime) ? (Long) startTime : System.currentTimeMillis();
         long requestFinishTime = System.currentTimeMillis();
         long cost = requestFinishTime - requestStartTime;
 
         String url = request.getRequestURL().toString();
-        String httpMethod = request.getMethod();
-        String remoteHost = IpUtil.getIpAddress(request);
-        int remotePort = request.getRemotePort();
+        String method = request.getMethod();
+        String host = IpUtil.getIpAddress(request);
+        int port = request.getRemotePort();
 
-        log.info(String.format("cost[%d],url[%s],httpMethod[%s],remoteHost[%s],remotePort[%d]," +
-                "request[%s],response[%s]", cost, url, httpMethod, remoteHost, remotePort, parameters, response));
+        log.info(String.format("%d|%s|%s|%s|%d|%s|%s", cost, method, url, host, port, parameters, response));
     }
 
-    protected boolean ignoreRequest(String methodName, String method) {
-        return ERROR_PATH_METHOD_NAME.equalsIgnoreCase(methodName) || HEAD.equalsIgnoreCase(method);
-    }
 }
